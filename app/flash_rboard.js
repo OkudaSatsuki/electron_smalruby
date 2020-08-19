632ed84492564f1e561592fbd5ddbee117835e9e
OkudaSatsuki <o.satsubon120905.os@icloud.com>
2020-08-19 22:45:24 +0900

    commit

--- app/flash_rboard.js
+++ app/flash_rboard.js
@@ -1,19 +1,25 @@
 const { spawn } = require('child_process');
 const div = document.getElementById('box11');
 const p = document.createElement('p');
-const ret = spawn('cd', ['resources', '&', 'cd','app', '&', 'rboard_compile_write.bat'] ,{ shell: true}); 
-ret.stderr.on('data', (err) => {
-    console.log(err); 
-    p.textContent += err;
-    p.textContent += "<br>";
-    div.innerHTML = p.textContent;  
-    var elementHtml = document.documentElement;
-    var bottom = elementHtml.scrollHeight - elementHtml.clientHeight;
-    scrollby(0, 300);
-})
-ret.stdout.on('data', (data) => {
-    p.textContent += data;
-    p.textContent += "<br>";
-    div.innerHTML = p.textContent;
-    scrollby(0, 300);       
-})
+const ret = spawn('cd', ['pic'] ,{ shell: true}); 
+exec('rboard_compile_write.bat',(stderr,stdout) => {
+    if(stderr) {
+        console.log(stderr); 
+        p.textContent += stderr;
+        p.textContent += "<br>";
+        div.innerHTML = p.textContent;  
+        var elementHtml = document.documentElement;
+        var bottom = elementHtml.scrollHeight - elementHtml.clientHeight;
+        scrollby(0, 300);
+    }
+    else if(stdout){
+        console.log(stdout);
+        p.textContent += data;
+        p.textContent += "<br>";
+        div.innerHTML = p.textContent;
+        scrollby(0, 300);    
+    }   
+    else{
+        console.log(err);
+    }
+});
